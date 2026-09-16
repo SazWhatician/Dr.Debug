c5db5f2ece6be760ad651f85393ab3776c1876df
SazWhatician <saswatmohanty029@gmail.com>
2026-09-16 19:22:00 +0530

    chore(release): bump Dr. Debug to v0.1.17 via LiveUpdate

--- release/dr-debug.standalone.js
+++ release/dr-debug.standalone.js
@@ -11599,7 +11599,7 @@ ${rawJson}
           <div class="dr-debug-settings-update-banner">
             <div class="dr-debug-update-meta">
               <span class="dr-debug-update-tag">OFFICIAL RELEASE</span>
-              <span class="dr-debug-update-version">Dr. Debug v0.1.16</span>
+              <span class="dr-debug-update-version">Dr. Debug v0.1.17</span>
             </div>
             <button type="button" id="dr-debug-btn-check-update" class="dr-debug-btn-update">
               <span>Check for Updates</span>
@@ -11812,7 +11812,7 @@ ${rawJson}
     async handleCheckUpdate(btn) {
       var _a, _b;
       const originalText = btn.innerHTML;
-      const currentVersion = "0.1.16";
+      const currentVersion = "0.1.17";
       btn.disabled = true;
       btn.innerHTML = `<span>Checking...</span>`;
       btn.style.opacity = "0.85";
